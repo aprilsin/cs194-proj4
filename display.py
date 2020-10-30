1af5b058aaa9fa88ed0ac72fd16fa29b6b5aa1e7
rainbowCat <jazziblue@gmail.com>
2020-10-29 22:22:47 -0700

    Do part 2 fist before part 1. Fix display.

--- display.py
+++ display.py
@@ -1,9 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 
 
-def show_keypoints(image, keypoints):
+def show_keypoints(data):
     """Show image with keypoints"""
+    assert type(data) == tuple, type(data)
+    assert len(data) == 2, len(data)
+
+    # unpack a sample data
+    image, keypoints = data
+    assert torch.is_tensor(image), type(image)
+
+    # change channels-first to channels-last format
+    image = image.permute(1, 2, 0)
+
+    # remove dimension of size 1 for plt
+    image = torch.squeeze(image)
+
+    # show image and plot keypoints
     plt.imshow(image, cmap="gray")
     plt.scatter(keypoints[:, 0], keypoints[:, 1], s=10, marker=".", c="r")
     plt.pause(0.001)  # pause a bit so that plots are updated
