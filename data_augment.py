1cc48d07de08aa677847adadc2db371e5d02808c
rainbowCat <jazziblue@gmail.com>
2020-10-31 04:45:49 -0700

    Clean up rotation

--- data_augment.py
+++ data_augment.py
@@ -1,27 +1,15 @@
+import math
 import typing
 from pathlib import Path
-from typing import (
-    Callable,
-    Dict,
-    FrozenSet,
-    Iterable,
-    List,
-    NamedTuple,
-    NewType,
-    Optional,
-    Sequence,
-    Set,
-    Tuple,
-    TypeVar,
-    Union,
-)
+from typing import (Callable, Dict, FrozenSet, Iterable, List, NamedTuple,
+                    NewType, Optional, Sequence, Set, Tuple, TypeVar, Union)
 
 import numpy as np
 import skimage.transform as ST
-from skimage import io
 import torch
-from torch import Tensor
 import torchvision.transforms as TT
+from skimage import io
+from torch import Tensor
 
 
 def part1_augment(image, keypoints) -> Tuple[Tensor, Tensor]:
@@ -38,8 +26,7 @@ def part1_augment(image, keypoints) -> Tuple[Tensor, Tensor]:
 
 def part2_augment(image, keypoints) -> Tuple[Tensor, Tensor]:
     # print(image.shape, keypoints.shape)
-    # pts = torch.unsqueeze(keypoints, 1)
-    # print(pts)
+
     # jitter = TT.ColorJitter(brightness=0.3)
     # image = jitter(image)
 
@@ -49,34 +36,19 @@ def part2_augment(image, keypoints) -> Tuple[Tensor, Tensor]:
     center = (h / 2, w / 2)
 
     rotate_deg = np.random.randint(-15, 15)
-
-    print(rotate_deg)
     image = ST.rotate(image, angle=rotate_deg, center=(0, 0))
-
-    print(keypoints[52])
     for i in range(len(keypoints)):
         point = keypoints[i]
         qx, qy = rotate(point, (0, 0), -rotate_deg)
         keypoints[i][0] = qx
         keypoints[i][1] = qy
-    print(keypoints[52])
 
     # convert back to tensors
     image = torch.from_numpy(image)
     image = torch.unsqueeze(image, 0)
     keypoints = torch.from_numpy(keypoints)
-    print(image.shape, keypoints.shape)
+    # print(image.shape, keypoints.shape)
     return image, keypoints
-
-
-def rotation_mat(rot_deg):
-    theta = np.radians(rot_deg)
-    c, s = np.cos(theta), np.sin(theta)
-    R = np.array(((c, -s), (s, c)))
-    return R
-
-
-import math
 
 
 def rotate(point, origin, angle):
