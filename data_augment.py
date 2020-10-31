1e2ca67fe788abb3af5304774499d91e87b31655
rainbowCat <jazziblue@gmail.com>
2020-10-31 03:38:03 -0700

    Add rotation matrix with center (affine)

--- data_augment.py
+++ data_augment.py
@@ -1,15 +1,30 @@
 import typing
 from pathlib import Path
-from typing import (Callable, Dict, FrozenSet, Iterable, List, NamedTuple,
-                    NewType, Optional, Sequence, Set, Tuple, TypeVar, Union)
+from typing import (
+    Callable,
+    Dict,
+    FrozenSet,
+    Iterable,
+    List,
+    NamedTuple,
+    NewType,
+    Optional,
+    Sequence,
+    Set,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
 import numpy as np
 import skimage.transform as ST
+from skimage import io
 import torch
+from torch import Tensor
 import torchvision.transforms as TT
 
 
-def part1_augment(image, keypoints):
+def part1_augment(image, keypoints) -> Tuple[Tensor, Tensor]:
     h, w = image.shape[-2:]
 
     # resize
@@ -21,30 +36,35 @@ def part1_augment(image, keypoints):
     return image, keypoints
 
 
-def part2_augment(image, keypoints):
+def part2_augment(image, keypoints) -> Tuple[Tensor, Tensor]:
+    # print(image.shape, keypoints.shape)
 
-    print(image.shape, keypoints.shape)
-    
-    jitter = TT.ColorJitter(brightness=0.3)
-    image = jitter(image)
+    # jitter = TT.ColorJitter(brightness=0.3)
+    # image = jitter(image)
 
     # convert tensors to numpy arrays to use skimage
-    image, keypoints = image.numpy(), keypoints.numpy()
+    image, keypoints = image.squeeze().numpy(), keypoints.numpy()
+    h, w = image.shape
 
-    rotate_deg = np.randint(-15, 15)
+    rotate_deg = np.random.randint(-15, 15)
     image = ST.rotate(image, rotate_deg)
-    keypoints = rotation_mat(rotate_deg) * keypoints.T
-    # rotate = TT.RandomRotation((-15, 15))
-    # print(rotate.get_params())
-    # image = rotate(image)
-    print(image.shape)
-    # keypoints = rotate(keypoints)
+    
+    R = rotation_mat(rotate_deg, center=(h/2, w/2))
+    print(R.shape, keypoints.T)
+    keypoints = (R @ keypoints.T).T
 
-    print(image.shape, keypoints.shape)
+    # convert back to tensors
+    image = torch.from_numpy(image)
+    image = torch.unsqueeze(image, 0)
+    keypoints = torch.from_numpy(keypoints)
+    # print(image.shape, keypoints.shape)
     return image, keypoints
 
-def rotation_mat(rot_deg):
+
+def rotation_mat(rot_deg, center):
+    center_h, center_w = center
+    center_x, center_y = center_w, center_h
     theta = np.radians(rot_deg)
     c, s = np.cos(theta), np.sin(theta)
-    R = np.array(((c, -s), (s, c)))
+    R = np.array(((c, -s, center_x - center_y), (s, c, center_x + center_y)))
     return R
