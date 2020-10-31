9a49420b4c8297c8cf15af4f11f12daf60603022
rainbowCat <jazziblue@gmail.com>
2020-10-30 17:33:19 -0700

    Can train part 1. Can do validation for part 1. Fixing show_keypoints to work with different shapes.

--- display.py
+++ display.py
@@ -16,25 +16,41 @@ def valid_keypoints(image, keypoints):
     ).all(), f"{cols.min() = }, {cols.max() = } out of bounds for {image.shape = }"
     return True
 
-def show_keypoints(data):
+
+def show_keypoints(
+    image: Union[Tensor, np.ndarray],
+    truth_points: Union[Tensor, np.ndarray],
+    pred_points: Union[Tensor, np.ndarray] = None,
+)->None:
     """Show image with keypoints"""
-    assert type(data) == tuple, type(data)
-    assert len(data) == 2, len(data)
+    
+    # check inputs have the correct shape
+    assert image.ndim == 2, image.shape
+    assert truth_points.ndim == 2 and truth_point.shape[1] == 2, truth_points.shape
+    if pred_points is not None:
+        assert pred_points.ndim == 2 and pred_point.shape[1] == 2, pred_points.shape
 
-    # unpack a sample data
-    image, keypoints = data
-    assert torch.is_tensor(image), type(image)
 
-    # change channels-first to channels-last format
-    image = image.permute(1, 2, 0)
+    assert image.ndim == 2, image.shape
+    assert truth_points.ndim == 2, truth_points.shape
+    # # change channels-first to channels-last format
+    # image = image.permute(1, 2, 0)
 
-    # remove dimension of size 1 for plt
-    image = torch.squeeze(image)
-
-    assert valid_keypoints(image, keypoints)
+    # # remove dimension of size 1 for plt
+    # image = torch.squeeze(image)
 
     # show image and plot keypoints
+    plt.figure()
     plt.imshow(image, cmap="gray")
-    plt.scatter(keypoints[:, 0], keypoints[:, 1], s=10, marker=".", c="g")
-    plt.pause(0.001)  # pause a bit so that plots are updated
+    assert valid_keypoints(image, truth_points)
+    plt.scatter(truth_points[:, 0], truth_points[:, 1], s=35, c="g", marker="x")
 
+    if pred_points is not None:
+        assert torch.is_tensor(pred_points), type(image)
+        pred_points = pred_points.detach().numpy()
+        assert pred_points.ndim == 2
+        assert valid_keypoints(image, pred_points)
+        plt.scatter(pred_points[:, 0], pred_points[:, 1], s=35, c="r", marker="x")
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    plt.show()
