9753e4d4f2b23853401f62f26d3f0514ef5b599a
rainbowCat <jazziblue@gmail.com>
2020-11-02 20:10:56 -0800

    Add code submission. Add xml for pictures of me :)

---
+++ submission_code/constants.py
@@ -0,0 +1,17 @@
+""" Global Variables """
+import torch
+
+
+NUM_POINTS = 41
+NUM_CHANNELS = 3
+NUM_FRAMES = 20
+DEFAULT_HEIGHT = 575
+DEFAULT_WIDTH = 547
+DEFAULT_EYE_LEN = DEFAULT_WIDTH * 0.25
+PAD_MODE = "edge"
+
+
+DANES_HEIGHT = 480
+DANES_WIDTH = 640
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
